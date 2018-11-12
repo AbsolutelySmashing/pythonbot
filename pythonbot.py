0143ffb0748147f8665126f108e4fe99bb7980bf
AbsolutelySmashing <36330313+AbsolutelySmashing@users.noreply.github.com>
2018-11-12 19:43:28 +0000

    5
    5

--- pythonbot.py
+++ pythonbot.py
@@ -2,18 +2,19 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import time
 
+#Initialise client bot
 Client = discord.Client() #Initialise Client 
-client = commands.Bot(command_prefix = "!") #Initialise client bot
+bot = commands.Bot(process.env.prefix) 
+bot.run(process.env.token)
 
 @client.event 
 async def on_ready():
-    print("Bot is online and connected to Discord") #This will be called when the bot connects to the server
+    #This will be called when the bot connects to the server
+    print("Bot is online and connected to Discord") 
 
 @client.event
 async def on_message(message):
+    #responds with Cookie emoji when someone says "cookie"
     if message.content == "cookie":
-        await message.channel.send(":cookie:") #responds with Cookie emoji when someone says "cookie"
-
-client.run(process.env.token)
+        await message.channel.send(":cookie:") 
