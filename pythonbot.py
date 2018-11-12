32f8148d7f571253763045775d955f8cc6720bad
AbsolutelySmashing <36330313+AbsolutelySmashing@users.noreply.github.com>
2018-11-12 20:38:15 +0000

    12
    12

--- pythonbot.py
+++ pythonbot.py
@@ -8,12 +8,12 @@ import asyncio
 Client = discord.Client() #Initialise Client 
 bot = commands.Bot(command_prefix = os.environ["prefix"]) 
 
-@client.event 
+@bot.event 
 async def on_ready():
     #This will be called when the bot connects to the server
     print("Bot is online and connected to Discord") 
 
-@client.event
+@bot.event
 async def on_message(message):
     #responds with Cookie emoji when someone says "cookie"
     if message.content == "cookie":
