f1f9e5bfa8ed685511929fdef8870766d5bc9065
AbsolutelySmashing <36330313+AbsolutelySmashing@users.noreply.github.com>
2018-11-13 23:27:12 +0000

    v0.1
    hello and cookie

--- pythonbot.py
+++ pythonbot.py
@@ -1,22 +1,26 @@
-import os
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
-import asyncio
+import settings
+#from discord.ext.commands import Bot
+#from discord.ext import commands
 
-#Initialise client bot
-Client = discord.Client() #Initialise Client 
-bot = commands.Bot(command_prefix = os.environ["prefix"]) 
+bot = discord.Client() #Initialise Client
+#bot = commands.Bot(command_prefix = os.environ["prefix"])
 
-@bot.event 
+# TODO: Once ready
+@bot.event
 async def on_ready():
-    #This will be called when the bot connects to the server
-    print("Bot is online and connected to Discord") 
+    print("Bot is online and connected to Discord")
 
+#TODO: When a message is sent on discord
 @bot.event
 async def on_message(message):
-    #responds with Cookie emoji when someone says "cookie"
-    if message.content == "cookie":
-        await bot.send_message(message.channel, ":cookie:") 
+    if message.author.bot:
+        return #ignore other bots
+
+    if "hello" in message.content.lower():
+        await  bot.send_message(message.channel, "Hello! :)")
+    if "cookie" in message.content.lower():
+        await bot.send_message(message.channel, ":cookie:")
 
 bot.run(os.environ["token"])
+#bot.run(settings.getToken())
